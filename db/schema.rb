341754dde4a1abf08868d94ff8c678de99058b59
gabrielmm1234 <gabrielmesquita@cjr.org.br>
2016-04-14 17:37:02 -0300

    pull

--- db/schema.rb
+++ db/schema.rb
@@ -49,8 +49,8 @@ ActiveRecord::Schema.define(version: 20160413193538) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
     t.integer  "profile_id"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
