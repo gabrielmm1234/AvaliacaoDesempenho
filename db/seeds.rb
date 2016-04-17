fbe4a762c019c408b3d5d15a947b3497871ceaca
Bruno Helder <brunohelder@cjr.org.br>
2016-04-17 17:35:07 -0300

    Adicionei a role e EJ do admin no seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,7 @@ User.delete_all
 
 admin = Profile.create name: 'Administrador'
 usuario_comum = Profile.create name: 'Usuário Comum'
+role = Role.create name: 'Cargo'
+junior_enterprise = JuniorEnterprise.create name: 'Empresa Júnior'
 
-User.create! name: 'admin', email: 'admin@admin.com', profile_id: admin.id, password: '12345678'
+User.create! name: 'admin', email: 'admin@admin.com', profile_id: admin.id, password: '12345678', role_id: role.id, junior_enterprise_id: junior_enterprise.id
