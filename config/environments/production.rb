7e9610b93bb2bc28827779fcddccb4ed5fa368b0
Bruno Helder <brunohelder@cjr.org.br>
2016-04-29 17:29:34 -0300

    Atualiza configurações de email para produção

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'avaliacaodesempenho.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :authentication => :plain,
+    :domain => 'gmail.com',
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["GMAIL_PASSWORD"]
+  }
+
 end
