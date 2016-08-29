616601a03116dc7049f152da4e34cfaacf68abce
luiz <luizfernandovieira_cf@hotmail.com>
2016-08-28 21:10:54 -0300

    resolvendo problemas misteriosos de postgres e depois device

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,24 +19,39 @@ class ApplicationController < ActionController::Base
     home_path
   end
 
-  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
+
+  def configure_permitted_parameters
+       registration_params = [:first_name, :profile_id, :email, :password, :password_confirmation]
+
+       if params[:action] == 'update'
+         devise_parameter_sanitizer.permit(:account_update) {
+           |u| u.permit(registration_params << :current_password)
+         }
+       elsif params[:action] == 'create'
+         devise_parameter_sanitizer.permit(:sign_up) {
+           |u| u.permit(registration_params)
+         }
+       end
+  end
+
   # Esses atributos podem ser adicionados nos controllers do Devise
 
-   def configure_devise_permitted_parameters
-     registration_params = [:first_name, :profile_id, :email, :password, :password_confirmation]
-
-     if params[:action] == 'update'
-       devise_parameter_sanitizer.for(:account_update) { 
-         |u| u.permit(registration_params << :current_password)
-       }
-     elsif params[:action] == 'create'
-       devise_parameter_sanitizer.for(:sign_up) { 
-         |u| u.permit(registration_params) 
-       }
-     end
-   end
+  #  def configure_devise_permitted_parameters
+  #    registration_params = [:first_name, :profile_id, :email, :password, :password_confirmation]
+   #
+  #    if params[:action] == 'update'
+  #      devise_parameter_sanitizer.for(:account_update) {
+  #        |u| u.permit(registration_params << :current_password)
+  #      }
+  #    elsif params[:action] == 'create'
+  #      devise_parameter_sanitizer.for(:sign_up) {
+  #        |u| u.permit(registration_params)
+  #      }
+  #    end
+  #  end
 
 end
